bc88a8bda03a34115d669e390e6b806dae718b9a
Sanskar-12 <sanskarv2004@gmail.com>
2026-02-14 17:03:48 +0530

    Feature: Added Resend to send the emails

--- services/email.service.js
+++ services/email.service.js
@@ -1,15 +1,25 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-const sendMail = (email, password, mailData) => {
-  const transport = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-      user: email,
-      pass: password,
-    },
-  });
+const sendMail = async (email, password, mailData) => {
+  // const transport = nodemailer.createTransport({
+  //   service: "Gmail",
+  //   auth: {
+  //     user: email,
+  //     pass: password,
+  //   },
+  // });
 
-  transport.sendMail({
+  // transport.sendMail({
+  //   from: mailData.from,
+  //   to: mailData.to,
+  //   subject: mailData.subject,
+  //   text: mailData.text,
+  // });
+
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
+  await resend.emails.send({
     from: mailData.from,
     to: mailData.to,
     subject: mailData.subject,
